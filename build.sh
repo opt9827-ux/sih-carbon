dde0e98cdf50d4de6bcbff33aacc83c41c32e5d0
opt9827-ux <opt9827@gmail.com>
2025-09-26 10:35:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,20 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python -c "from app import app, db, User; app.app_context().push(); db.create_all(); admin = User.query.filter_by(role='admin').first(); \
-if not admin: \
-  admin_user = User(email='palapalaprasanth@gmial.com', role='admin'); \
-  admin_user.set_password('12345678'); \
-  db.session.add(admin_user); \
-  db.session.commit(); \
-  print('Admin user created.')"
+python -c "
+from app import app, db, User
+
+print('--- Initializing Database ---')
+with app.app_context():
+    db.create_all()
+    admin = User.query.filter_by(role='admin').first()
+    if not admin:
+        print('--- Creating Admin User ---')
+        admin_user = User(email='palapalaprasanth@gmial.com', role='admin')
+        admin_user.set_password('12345678')
+        db.session.add(admin_user)
+        db.session.commit()
+        print('Admin user created successfully.')
+    else:
+        print('Admin user already exists.')
+"
